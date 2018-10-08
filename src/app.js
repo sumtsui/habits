e7898ff48546f3fe9639d4f34357ff8c6f6cbecc
Sum Tsui <sumtsui@gmail.com>
2018-10-08 20:59:54 +0800

    commit

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ MongoClient.connect(config.db.mlab_url, {useNewUrlParser: true}, (err, client) =
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 1 week
     store: new MongoStore({ db }), // Save session in DB
     name: 'habits',
-    cookie: { httpOnly: false, secure: true },
+    cookie: { httpOnly: false },
   }));
 
   // Make DB instance avaialbe in req
