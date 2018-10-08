276113642d2a1682298e7ad843d8924e704e2e27
Sum Tsui <sumtsui@gmail.com>
2018-10-08 22:18:31 +0800

    commit

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the Habit API'
-  })
+  // res.json({
+  //   message: 'Welcome to the Habit API'
+  // })
+  res.set({
+    'x-auth-token': 'fuck',
+    'Location': '/'
+  }).send(200);
 })
 
 module.exports = router;
