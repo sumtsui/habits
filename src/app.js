eaca6b81e4c852e4ced8ed2ce4b3a636e1686470
Sum Tsui <sumtsui@gmail.com>
2018-10-08 23:42:45 +0800

    commit

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // // cater CROS preflight request
 app.options("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Origin", 'https://habit-logging-app.herokuapp.com');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -30,7 +30,7 @@ app.options("*", function (req, res, next) {
 
 // // Allow CROS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Origin", 'https://habit-logging-app.herokuapp.com');
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
